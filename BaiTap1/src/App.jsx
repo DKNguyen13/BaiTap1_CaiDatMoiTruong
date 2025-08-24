dd63011ac4079293c2d817bf5be43b8ea912dfdb
nguyen.dk <nd2004lk13@gmail.com>
2025-08-24 15:10:54 +0700

    22110383_DuongKhanhNguyen

---
+++ BaiTap1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Xin chào, mình là Dương Khánh Nguyên</h1>
+      <p>Trường: HCMUTE</p>
+      <p>Sở thích: Lập trình MERN stack</p>
+    </div>
+  );
+}
+
+export default App
